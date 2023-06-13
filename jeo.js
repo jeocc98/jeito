c324b43acb4c9fda5f122ee34d8738fa25b56747
Jeo Cornejo <jeocorne@hotmail.com>
2023-06-13 15:57:00 -0500

    tiempo1

---
+++ jeo.js
@@ -0,0 +1,11 @@
+
+function dibujar(){
+
+var x1 = parseInt(document.getElementById("x1").value);
+var y1 = parseInt(document.getElementById("y1").value);
+var x2 = parseInt(document.getElementById("x2").value);
+var y2 = parseInt(document.getElementById("y2").value);
+
+r=x1+x2+y1+y2
+document.write("la sumas es "+r);
+}
